11c347bfb265e201965d8c2b26620e99f45bc634
Voytex <30312538+Voytex@users.noreply.github.com>
2019-03-13 12:59:35 +0100

    Add files via upload
    Added text fields for input data

--- sliders.js
+++ sliders.js
@@ -17,6 +17,7 @@ landing.oninput = function(){
 	if (landing.value == 0) landingOutput.innerHTML = "no";
   else landingOutput.innerHTML = "yes";
   launch(); 
+	console.log("landing: " + landing.value);
 }
 
 var units = document.getElementById("units");
@@ -33,218 +34,350 @@ units.oninput = function(){
 //stage 1
 var vesselCapacity = document.getElementById("vesselCapacity");
 var vesselCapacityOutput = document.getElementById("vesselCapacityValue");
-vesselCapacityOutput.innerHTML = vesselCapacity.value;
+vesselCapacityOutput.value = vesselCapacity.value;
 
 vesselCapacity.oninput = function(){
-	vesselCapacityOutput.innerHTML = vesselCapacity.value;
+	vesselCapacityOutput.value = vesselCapacity.value;
 	waterPercentage.innerHTML =  Math.round(waterCapacity.value / vesselCapacity.value * 100);
   launch(); 
 }
 
+vesselCapacityOutput.onchange = function(){
+	vesselCapacity.value = vesselCapacityOutput.value;
+	waterPercentage.innerHTML =  Math.round(waterCapacity.value / vesselCapacity.value * 100);
+	launch();
+}
+
 var waterCapacity = document.getElementById("waterCapacity");
 var waterCapacityOutput = document.getElementById("waterCapacityValue");
-waterCapacityOutput.innerHTML = waterCapacity.value;
+waterCapacityOutput.value = waterCapacity.value;
 var waterPercentage = document.getElementById("waterPercentage");
 waterPercentage.innerHTML =  Math.round(waterCapacity.value / vesselCapacity.value * 100);
 
 waterCapacity.oninput = function(){
-	waterCapacityOutput.innerHTML = waterCapacity.value;
+	waterCapacityOutput.value = waterCapacity.value;
   waterPercentage.innerHTML =  Math.round(waterCapacity.value / vesselCapacity.value * 100);
 	launch(); 
 }
 
+waterCapacityOutput.onchange = function(){
+	waterCapacity.value = waterCapacityOutput.value;
+	waterPercentage.innerHTML =  Math.round(waterCapacity.value / vesselCapacity.value * 100);
+	launch();
+}
+
 var rocketDryMass = document.getElementById("rocketDryMass");
 var rocketDryMassOutput = document.getElementById("rocketDryMassValue");
-rocketDryMassOutput.innerHTML = rocketDryMass.value;
+rocketDryMassOutput.value = rocketDryMass.value;
 
 rocketDryMass.oninput = function(){
-	rocketDryMassOutput.innerHTML = rocketDryMass.value;
+	rocketDryMassOutput.value = rocketDryMass.value;
   launch(); 
+}
+
+rocketDryMassOutput.onchange = function(){
+	rocketDryMass.value = rocketDryMassOutput.value;
+	launch();
 }
 
 var pressure = document.getElementById("pressure");
 var pressureOutput = document.getElementById("pressureValue");
-pressureOutput.innerHTML = pressure.value;
+pressureOutput.value = pressure.value;
 
 pressure.oninput = function(){
-	pressureOutput.innerHTML = pressure.value;
-  launch(); }
+	pressureOutput.value = pressure.value;
+  launch(); 
+}
+
+pressureOutput.onchange = function(){
+	pressure.value = pressureOutput.value;
+  launch(); 
+}
 
 var dragCoefficient = document.getElementById("dragCoefficient");
 var dragCoefficientOutput = document.getElementById("dragCoefficientValue");
-dragCoefficientOutput.innerHTML = dragCoefficient.value;
+dragCoefficientOutput.value = dragCoefficient.value;
 
 dragCoefficient.oninput = function(){
-	dragCoefficientOutput.innerHTML = dragCoefficient.value;
-  launch(); }
+	dragCoefficientOutput.value = dragCoefficient.value;
+  launch(); 
+}
+
+dragCoefficientOutput.onchange = function(){
+	dragCoefficient.value = dragCoefficientOutput.value;
+	launch();
+}
 
 var vesselDiameter = document.getElementById("vesselDiameter");
 var vesselDiameterOutput = document.getElementById("vesselDiameterValue");
-vesselDiameterOutput.innerHTML = vesselDiameter.value;
+vesselDiameterOutput.value = vesselDiameter.value;
 
 vesselDiameter.oninput = function(){
-	vesselDiameterOutput.innerHTML = vesselDiameter.value;
-  launch(); }
+	vesselDiameterOutput.value = vesselDiameter.value;
+  launch(); 
+}
+
+vesselDiameterOutput.onchange = function(){
+	vesselDiameter.value = vesselDiameterOutput.value;
+	launch();
+}
 
 var nozzleDiameter = document.getElementById("nozzleDiameter");
 var nozzleDiameterOutput = document.getElementById("nozzleDiameterValue");
-nozzleDiameterOutput.innerHTML = nozzleDiameter.value;
+nozzleDiameterOutput.value = nozzleDiameter.value;
 
 nozzleDiameter.oninput = function(){
-	nozzleDiameterOutput.innerHTML = nozzleDiameter.value;
-  launch(); }
+	nozzleDiameterOutput.value = nozzleDiameter.value;
+  launch(); 
+}
+
+nozzleDiameterOutput.onchange = function(){
+	nozzleDiameter.value = nozzleDiameterOutput.value;
+	launch();
+}
 
 var reserve = document.getElementById("reserve");
 var reserveOutput = document.getElementById("reserveValue");
-reserveOutput.innerHTML = reserve.value;
+reserveOutput.value = reserve.value;
 
 reserve.oninput = function(){
-	reserveOutput.innerHTML = reserve.value;
-  launch(); }
+	reserveOutput.value = reserve.value;
+  launch(); 
+}
+
+reserveOutput.onchange = function(){
+	reserve.value = reserveOutput.value;
+	launch();
+}
 
 var launchTubeLength = document.getElementById("launchTubeLength");
 var launchTubeLengthOutput = document.getElementById("launchTubeLengthValue");
-launchTubeLengthOutput.innerHTML = launchTubeLength.value;
+launchTubeLengthOutput.value = launchTubeLength.value;
 
 launchTubeLength.oninput = function(){
-	launchTubeLengthOutput.innerHTML = launchTubeLength.value;
-  launch(); }
+	launchTubeLengthOutput.value = launchTubeLength.value;
+  launch(); 
+}
+
+launchTubeLengthOutput.onchange = function(){
+	launchTubeLength.value = launchTubeLengthOutput.value;
+	launch();
+}
 
 var launchTubeDiameter = document.getElementById("launchTubeDiameter");
 var launchTubeDiameterOutput = document.getElementById("launchTubeDiameterValue");
-launchTubeDiameterOutput.innerHTML = launchTubeDiameter.value;
+launchTubeDiameterOutput.value = launchTubeDiameter.value;
 
 launchTubeDiameter.oninput = function(){
-	launchTubeDiameterOutput.innerHTML = launchTubeDiameter.value;
-  launch(); }
+	launchTubeDiameterOutput.value = launchTubeDiameter.value;
+  launch(); 
+}
+
+launchTubeDiameterOutput.onchange = function(){
+	launchTubeDiameter.value = launchTubeDiameterOutput.value;
+	launch();
+}
 
 
 //same for stage 2
 
+
 var vesselCapacity2 = document.getElementById("vesselCapacity2");
 var vesselCapacityOutput2 = document.getElementById("vesselCapacityValue2");
-vesselCapacityOutput2.innerHTML = vesselCapacity2.value;
+vesselCapacityOutput2.value = vesselCapacity2.value;
 
 vesselCapacity2.oninput = function(){
-	vesselCapacityOutput2.innerHTML = vesselCapacity2.value;
+	vesselCapacityOutput2.value = vesselCapacity2.value;
 	waterPercentage2.innerHTML =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
   launch(); }
+
+vesselCapacityOutput2.onchange = function(){
+	vesselCapacity2.value = vesselCapacityOutput2.value;
+	waterPercentage2.innerHTML =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
+	launch();
+}
 
 var waterCapacity2 = document.getElementById("waterCapacity2");
 var waterCapacityOutput2 = document.getElementById("waterCapacityValue2");
-waterCapacityOutput2.innerHTML = waterCapacity2.value;
+waterCapacityOutput2.value = waterCapacity2.value;
 var waterPercentage2 = document.getElementById("waterPercentage2");
-waterPercentage2.innerHTML =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
+waterPercentage2.value =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
 
 waterCapacity2.oninput = function(){
-	waterCapacityOutput2.innerHTML = waterCapacity2.value;
+	waterCapacityOutput2.value = waterCapacity2.value;
 	waterPercentage2.innerHTML =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
   launch(); }
 
+waterCapacityOutput2.onchange = function(){
+	waterCapacity2.value = waterCapacityOutput2.value;
+	waterPercentage2.innerHTML =  Math.round(waterCapacity2.value / vesselCapacity2.value * 100);
+	launch();
+}
+
 var rocketDryMass2 = document.getElementById("rocketDryMass2");
 var rocketDryMassOutput2 = document.getElementById("rocketDryMassValue2");
-rocketDryMassOutput2.innerHTML = rocketDryMass2.value;
+rocketDryMassOutput2.value = rocketDryMass2.value;
 
 rocketDryMass2.oninput = function(){
-	rocketDryMassOutput2.innerHTML = rocketDryMass2.value;
+	rocketDryMassOutput2.value = rocketDryMass2.value;
   launch(); }
+
+rocketDryMassOutput2.onchange = function(){
+	rocketDryMass2.value = rocketDryMassOutput2.value;
+	launch();
+}
 
 var pressure2 = document.getElementById("pressure2");
 var pressureOutput2 = document.getElementById("pressureValue2");
-pressureOutput2.innerHTML = pressure2.value;
+pressureOutput2.value = pressure2.value;
 
 pressure2.oninput = function(){
-	pressureOutput2.innerHTML = pressure2.value;
+	pressureOutput2.value = pressure2.value;
   launch(); }
+
+pressureOutput2.onchange = function(){
+	pressure2.value = pressureOutput2.value;
+  launch(); 
+}
 
 var dragCoefficient2 = document.getElementById("dragCoefficient2");
 var dragCoefficientOutput2 = document.getElementById("dragCoefficientValue2");
-dragCoefficientOutput2.innerHTML = dragCoefficient2.value;
+dragCoefficientOutput2.value = dragCoefficient2.value;
 
 dragCoefficient2.oninput = function(){
-	dragCoefficientOutput2.innerHTML = dragCoefficient2.value;
+	dragCoefficientOutput2.value = dragCoefficient2.value;
   launch(); }
+
+dragCoefficientOutput2.onchange = function(){
+	dragCoefficient2.value = dragCoefficientOutput2.value;
+	launch();
+}
 
 var vesselDiameter2 = document.getElementById("vesselDiameter2");
 var vesselDiameterOutput2 = document.getElementById("vesselDiameterValue2");
-vesselDiameterOutput2.innerHTML = vesselDiameter2.value;
+vesselDiameterOutput2.value = vesselDiameter2.value;
 
 vesselDiameter2.oninput = function(){
-	vesselDiameterOutput2.innerHTML = vesselDiameter2.value;
+	vesselDiameterOutput2.value = vesselDiameter2.value;
   launch(); }
+
+vesselDiameterOutput2.onchange = function(){
+	vesselDiameter2.value = vesselDiameterOutput2.value;
+	launch();
+}
 
 var nozzleDiameter2 = document.getElementById("nozzleDiameter2");
 var nozzleDiameterOutput2 = document.getElementById("nozzleDiameterValue2");
-nozzleDiameterOutput2.innerHTML = nozzleDiameter2.value;
+nozzleDiameterOutput2.value = nozzleDiameter2.value;
 
 nozzleDiameter2.oninput = function(){
-	nozzleDiameterOutput2.innerHTML = nozzleDiameter2.value;
+	nozzleDiameterOutput2.value = nozzleDiameter2.value;
   launch(); }
+
+nozzleDiameterOutput2.onchange = function(){
+	nozzleDiameter2.value = nozzleDiameterOutput2.value;
+	launch();
+}
 
 
 //same for stage 3
 
 var vesselCapacity3 = document.getElementById("vesselCapacity3");
 var vesselCapacityOutput3 = document.getElementById("vesselCapacityValue3");
-vesselCapacityOutput3.innerHTML = vesselCapacity3.value;
+vesselCapacityOutput3.value = vesselCapacity3.value;
 
 vesselCapacity3.oninput = function(){
-	vesselCapacityOutput3.innerHTML = vesselCapacity3.value;
+	vesselCapacityOutput3.value = vesselCapacity3.value;
 	waterPercentage3.innerHTML =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
-  launch(); 
+  launch(); }
+
+vesselCapacityOutput3.onchange = function(){
+	vesselCapacity3.value = vesselCapacityOutput3.value;
+	waterPercentage3.innerHTML =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
+	launch();
 }
 
 var waterCapacity3 = document.getElementById("waterCapacity3");
 var waterCapacityOutput3 = document.getElementById("waterCapacityValue3");
-waterCapacityOutput3.innerHTML = waterCapacity3.value;
+waterCapacityOutput3.value = waterCapacity3.value;
 var waterPercentage3 = document.getElementById("waterPercentage3");
-waterPercentage3.innerHTML =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
+waterPercentage3.value =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
 
 waterCapacity3.oninput = function(){
-	waterCapacityOutput3.innerHTML = waterCapacity3.value;
+	waterCapacityOutput3.value = waterCapacity3.value;
 	waterPercentage3.innerHTML =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
-  launch(); 
+  launch(); }
+
+waterCapacityOutput3.onchange = function(){
+	waterCapacity3.value = waterCapacityOutput3.value;
+	waterPercentage3.innerHTML =  Math.round(waterCapacity3.value / vesselCapacity3.value * 100);
+	launch();
 }
 
 var rocketDryMass3 = document.getElementById("rocketDryMass3");
 var rocketDryMassOutput3 = document.getElementById("rocketDryMassValue3");
-rocketDryMassOutput3.innerHTML = rocketDryMass3.value;
+rocketDryMassOutput3.value = rocketDryMass3.value;
 
 rocketDryMass3.oninput = function(){
-	rocketDryMassOutput3.innerHTML = rocketDryMass3.value;
+	rocketDryMassOutput3.value = rocketDryMass3.value;
   launch(); }
+
+rocketDryMassOutput3.onchange = function(){
+	rocketDryMass3.value = rocketDryMassOutput3.value;
+	launch();
+}
 
 var pressure3 = document.getElementById("pressure3");
 var pressureOutput3 = document.getElementById("pressureValue3");
-pressureOutput3.innerHTML = pressure3.value;
+pressureOutput3.value = pressure3.value;
 
 pressure3.oninput = function(){
-	pressureOutput3.innerHTML = pressure3.value;
+	pressureOutput3.value = pressure3.value;
   launch(); }
+
+pressureOutput3.onchange = function(){
+	pressure3.value = pressureOutput3.value;
+  launch(); 
+}
 
 var dragCoefficient3 = document.getElementById("dragCoefficient3");
 var dragCoefficientOutput3 = document.getElementById("dragCoefficientValue3");
-dragCoefficientOutput3.innerHTML = dragCoefficient3.value;
+dragCoefficientOutput3.value = dragCoefficient3.value;
 
 dragCoefficient3.oninput = function(){
-	dragCoefficientOutput3.innerHTML = dragCoefficient3.value;
+	dragCoefficientOutput3.value = dragCoefficient3.value;
   launch(); }
+
+dragCoefficientOutput3.onchange = function(){
+	dragCoefficient3.value = dragCoefficientOutput3.value;
+	launch();
+}
 
 var vesselDiameter3 = document.getElementById("vesselDiameter3");
 var vesselDiameterOutput3 = document.getElementById("vesselDiameterValue3");
-vesselDiameterOutput3.innerHTML = vesselDiameter3.value;
+vesselDiameterOutput3.value = vesselDiameter3.value;
 
 vesselDiameter3.oninput = function(){
-	vesselDiameterOutput3.innerHTML = vesselDiameter3.value;
+	vesselDiameterOutput3.value = vesselDiameter3.value;
   launch(); }
+
+vesselDiameterOutput3.onchange = function(){
+	vesselDiameter3.value = vesselDiameterOutput3.value;
+	launch();
+}
 
 var nozzleDiameter3 = document.getElementById("nozzleDiameter3");
 var nozzleDiameterOutput3 = document.getElementById("nozzleDiameterValue3");
-nozzleDiameterOutput3.innerHTML = nozzleDiameter3.value;
+nozzleDiameterOutput3.value = nozzleDiameter3.value;
 
 nozzleDiameter3.oninput = function(){
-	nozzleDiameterOutput3.innerHTML = nozzleDiameter3.value;
+	nozzleDiameterOutput3.value = nozzleDiameter3.value;
   launch(); }
+
+nozzleDiameterOutput3.onchange = function(){
+	nozzleDiameter3.value = nozzleDiameterOutput3.value;
+	launch();
+}
 
 
 
